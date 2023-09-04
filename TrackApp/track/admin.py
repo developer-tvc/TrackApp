9fc1a2258b1b7ccb601074ef524702d807bf4417
anuraj-kr <anurajkr23@gmail.com>
2023-09-04 11:51:49 +0530

    added visitor and unit name

--- TrackApp/track/admin.py
+++ TrackApp/track/admin.py
@@ -11,9 +11,17 @@ class UnitAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 class VisitAdmin(admin.ModelAdmin):
-    list_display = ('visit_date', 'longitude', 'latitude')
+    list_display = ('visit_date', 'longitude', 'latitude', 'get_visitor', 'get_unit')
     search_fields = ['unit__name', 'unit__worker__name']
 
+    @admin.display(description='Visitor')
+    def get_visitor(self, obj):
+        return obj.unit.worker.name
+
+    @admin.display(description='Unit')
+    def get_unit(self, obj):
+        return obj.unit.name
+    
     def has_add_permission(self, request, obj=None):
         return False
 
